1da47f43915cda08166b5b336aeb17fa0793f6a4
Abdul Qureshi <abdul.qureshi@digital.justice.gov.uk>
2022-10-18 18:07:05 +0100

    Working version of the file finder.

--- robot_scripts/Support/find_in_files.py
+++ robot_scripts/Support/find_in_files.py
@@ -1,23 +1,57 @@
 import os
 from robot.libraries.BuiltIn import BuiltIn
 
-def find_unused_images_in_directory(image_directory, task_directory):
+
+def get_all_images(image_directory):
+    images = []
+
     directory = os.fsencode(image_directory)
     for image in os.listdir(directory):
         found = False
-        # BuiltIn().log_to_console(image)
         imageStr = image.decode('UTF-8')
+        if os.path.isfile(os.path.join(image_directory, imageStr)):
+            images.append(imageStr)
+        elif os.path.isdir(image_directory + imageStr):
+            images.extend(get_all_images(image_directory + imageStr))
 
+    return images
+
+def get_all_files(task_directories):
+    files = []
+
+    for task_directory in task_directories:
         directory = os.fsencode(task_directory)
         for taskFile in os.listdir(directory):
-            BuiltIn().log_to_console(taskFile)
             taskFileStr = taskFile.decode('UTF-8')
             if taskFileStr.endswith('.robot'):
-                with open(os.path.join(task_directory, taskFileStr)) as f:
-                    if imageStr.lower() in f.read().lower():
-                        # Found the image referenced in a file, skip for image.
-                        found = True
-                        continue
+                filePath = os.path.join(task_directory, taskFileStr)
+                files.append(filePath)
+
+    return files
+
+def find_unused_images_in_directory(image_directory, task_directories):
+    images = get_all_images(image_directory)
+    files = get_all_files(task_directories)
+
+    BuiltIn().log_to_console("Images count: ")
+    BuiltIn().log_to_console(len(images))
+
+    BuiltIn().log_to_console("Robot files count: ")
+    BuiltIn().log_to_console(len(files))
+
+    count = 0
+    for image in images:
+        found = False
+
+        for taskFile in files:
+            with open(taskFile) as f:
+                contents = f.read().lower()
+                if contents.find(image.lower()) >= 0:
+                    found = True
+                    break
 
         if found == False:
-            BuiltIn().log_to_console("Unused image: " + imageStr)
+            BuiltIn().log_to_console("Unused image: " + image)
+            count+=1
+
+    BuiltIn().log_to_console("Unused image count: " + str(count) + " (" + str(round(count/len(images)*100)) + "%)")
--- robot_scripts/tasks/flag_unused_images.robot
+++ robot_scripts/tasks/flag_unused_images.robot
@@ -1,6 +1,10 @@
 *** Settings ***
 Library     ../Support/find_in_files.py
 
+*** Variables ***
+@{directories_to_scan}  robot_scripts/PageObjects
+${images_directory}  robot_scripts/Images/
+
 *** Tasks ***
 Flag Unused Images
-    find_in_files.find_unused_images_in_directory  robot_scripts/Images/  robot_scripts/tasks/
+    find_in_files.find_unused_images_in_directory  ${images_directory}  ${directories_to_scan}
